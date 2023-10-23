c0b74c34494ce2e85cfc06c5f8364bd872266ab9
DanielTvyt <daniel.voss@gmx.de>
2023-10-24 01:52:14 +0200

    Added first Item (Ruby)

--- src/main/java/net/daniel/tutorialmod/TutorialMod.java
+++ src/main/java/net/daniel/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package net.daniel.tutorialmod;
 
+import net.daniel.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -13,7 +14,7 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
-
 		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }

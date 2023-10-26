9b7020a8ae73224758894ac735040bf0db57e6b9
DanielTvyt <daniel.voss@gmx.de>
2023-10-26 22:22:26 +0200

    Added custom Item: Metal Detector (0.0.3)

--- src/main/java/net/daniel/tutorialmod/TutorialMod.java
+++ src/main/java/net/daniel/tutorialmod/TutorialMod.java
@@ -11,7 +11,7 @@ import org.slf4j.LoggerFactory;
 public class TutorialMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	public static final String MOD_ID = "tutorialmod";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
